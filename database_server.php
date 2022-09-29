8d33cf36c7f60138da9f5809665bb1c6a4b2d19a
Jennifer Neto <jn33@njit.edu>
2022-09-29 14:18:19 -0400

    dbserver name change

--- database_server.php
+++ database_server.php
@@ -91,7 +91,7 @@ function requestProcessor($request)
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
 
-$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+$server = new rabbitMQServer("testRabbitMQ.ini","dbServer");
 
 echo "testRabbitMQServer BEGIN".PHP_EOL;
 $server->process_requests('requestProcessor');
