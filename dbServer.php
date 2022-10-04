1fa2e9ded320b3c408310f86ce181a286952b3f3
Jennifer Neto <jn33@njit.edu>
2022-10-04 12:08:25 -0400

    changes to db.php

--- dbServer.php
+++ dbServer.php
@@ -5,75 +5,76 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
 
-
-//create connection
-$conn = new mysqli("172.23.51.116", "testUser", "12345", "testdb");
-
-
-// Check connection
-if ($conn->connect_error)
-{
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-
 function doLogin($username,$password)
-{
-    //to prevent from mysqli injection  
-        $username = stripcslashes($username);  
-        $password = stripcslashes($password);  
-        $username = mysqli_real_escape_string($conn, $username);  
-	$password = mysqli_real_escape_string($conn, $password);
+{    
+  //database connection
+  //create connection
+ 
+  $conn = new mysqli('127.0.0.1', 'testUser', '12345', 'testdb');
 
-    // lookup username in database	
-	$sql = "SELECT *from 'users' where username = '$username' and password = '$password'"; 
-	$result = mysqli_query($conn, $sql);  
+ 
+  // Check connection
+  if ($conn->connect_error)
+  {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully\n\n";
+
+
+// lookup username in database
+$query = "SELECT *FROM users where username = '$username' and password = '$password'";
+$result = mysqli_query($conn, $query);  
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
         $count = mysqli_num_rows($result);  
-          
-	if($count == 1)
-	{
+         
+if($count == 1)
+{
            echo "Login succesful!";
-	   return true;
-	}
-	else
-	{
-	   echo" login unsuccesful bicho";
-	   return false;
-	}
+  return "\nLogin succesful!";
+}
+else
+{
+  echo" login unsuccesful bicho";
+  return "\nLogin unsuccesful bitcho";
+}
 }
 
-function createUser($username, $password)
+function createUser($UserID, $username, $password)
 {
-	//to prevent from mysqli injection  
-        $username = stripcslashes($username);
-        $password = stripcslashes($password);
-        $username = mysqli_real_escape_string($conn, $username);
-        $password = mysqli_real_escape_string($conn, $password);
+//database connection
+  //create connection
+
+  $conn = new mysqli('127.0.0.1', 'testUser', '12345', 'testdb');
+
+
+  // Check connection
+  if ($conn->connect_error)
+  {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully\n\n";
 
     // lookup username in database      
-        $sql = "SELECT *from 'users' where username = '$username' and password = '$password'";
-        $result = mysqli_query($conn, $sql);
+        $query = "SELECT *FROM users where username = '$username' and password = '$password'";
+        $result = mysqli_query($conn, $query);
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
         $count = mysqli_num_rows($result);
 
         if($count == 1)
         {
-           echo "User already exists!";
-           return false;
+           echo "\nUser already exists!";
+           return "\nUser already exists";
         }
         else
         {
-	   $sql = "INSERT into `users` (username, password)
- 		   VALUES ('$username',($password')";
-	
-	   $result   = mysqli_query($con, $query);	
-	   echo "User created succesfully";
-           return true;
-	}
-}
+  $registerQuery = "INSERT into users
+  VALUES ('$UserID', '$username', '$password')";
 
+  $result   = mysqli_query($conn, $registerQuery);
+  echo "\nUser created succesfully";
+           return "\nUser created succesfully";
+        }
+}
 function requestProcessor($request)
 {
   echo "received request".PHP_EOL;
@@ -84,19 +85,25 @@ function requestProcessor($request)
   }
   switch ($request['type'])
   {
-    case "login":
+  case "login":
+      echo"login request recieved\n\n";
       return doLogin($request['username'],$request['password']);
-    case "validate_session":
+ 
+  case "validate_session":
+      echo "Session request recieved\n\n";
       return doValidate($request['sessionId']);
+ 
+  case "create_user":
+ echo "Create User request recieved\n\n";
+      return createUser($request['UserID'],$request['username'],$request['password']);
   }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+  return array("returnCode" => '0', 'message'=>"Server received request and processed\n\n");
 }
 
 $server = new rabbitMQServer("testRabbitMQ.ini","dbServer");
 
-echo "testRabbitMQServer BEGIN".PHP_EOL;
+echo "Database Server BEGIN\n\n".PHP_EOL;
 $server->process_requests('requestProcessor');
 echo "testRabbitMQServer END".PHP_EOL;
 exit();
 ?>
-
